9a3c9c3a9aefd1724490de94897d47b980f43706
Nikolaos Fragos <nifragos@gmail.com>
2016-02-07 11:15:12 +0200

    Exercise Prism Part 1

---
+++ KnolwdgeBase.Infrastructure/RegionNames.cs
@@ -0,0 +1,8 @@
+﻿namespace KnolwdgeBase.Infrastructure
+{
+    public class RegionNames
+    {
+        public static string ToolbarRegion = "ToolbarRegion";
+        public static string ContentRegion = "ContentRegion";
+    }
+}
